c45e581086f758f065f730d22417cbead9fb2444
Eyre_S <sukazyo@outlook.com>
2024-07-26 23:00:39 +0800

    fix dropWhileClass java capability

--- src/main/scala/stacks/StacksExtensions.scala
+++ src/main/scala/stacks/StacksExtensions.scala
@@ -1,39 +1,61 @@
 package cc.sukazyo.std
 package stacks
 
-import stacks.WithCurrentStack.EmptyStackException
-
 import scala.reflect.{classTag, ClassTag}
 
 object StacksExtensions {
 	
-	implicit class StacksExtensions (stacks: Array[StackTraceElement]) {
+	/** todo docs
+	  *
+	  * @since 0.2.0
+	  */
+	@throws[SecurityException]
+	def dropWhileClass (className: String)(stacks: Array[StackTraceElement]): Array[StackTraceElement] = {
+		stacks.dropWhile { stack => stack.getClassName == className }
+	}
+	
+	/** todo docs
+	  *
+	  * @since 0.2.0
+	  */
+	@throws[SecurityException]
+	def dropWhileClass (clazz: Class[?])(stacks: Array[StackTraceElement]): Array[StackTraceElement] = {
+		dropWhileClass(clazz.getName.asInstanceOf[String])(stacks)
+	}
+	
+	/** todo docs
+	  *
+	  * @since 0.2.0
+	  */
+	@throws[SecurityException]
+	def dropWhileClass[T: ClassTag] (stacks: Array[StackTraceElement]): Array[StackTraceElement] =
+		dropWhileClass(classTag[T].runtimeClass)(stacks)
+	
+	implicit class StackTrackArrayExt (stacks: Array[StackTraceElement]) {
 		
 		/** todo docs
 		  *
 		  * @since 0.2.0
 		  */
-		@throws[SecurityException | EmptyStackException]
-		def dropWhileClass (className: String): Array[StackTraceElement] = {
-			stacks.dropWhile { stack => stack.getClassName == className }
-		}
+		@throws[SecurityException]
+		def dropWhileClass (className: String): Array[StackTraceElement] =
+			StacksExtensions.dropWhileClass(className)(stacks)
 		
 		/** todo docs
 		  *
 		  * @since 0.2.0
 		  */
-		@throws[SecurityException | EmptyStackException]
-		def dropWhileClass (clazz: Class[?]): Array[StackTraceElement] = {
-			stacks.dropWhileClass(clazz.getName.nn)
-		}
+		@throws[SecurityException]
+		def dropWhileClass (clazz: Class[?]): Array[StackTraceElement] =
+			StacksExtensions.dropWhileClass(clazz)(stacks)
 		
 		/** todo docs
 		  * 
 		  * @since 0.2.0
 		  */
-		@throws[SecurityException | EmptyStackException]
+		@throws[SecurityException]
 		def dropWhileClass [T: ClassTag]: Array[StackTraceElement] =
-			this.dropWhileClass(classTag[T].runtimeClass)
+			StacksExtensions.dropWhileClass[T](stacks)
 		
 	}
 	
